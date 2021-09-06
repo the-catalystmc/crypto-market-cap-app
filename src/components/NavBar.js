08fc52f0f0be43968e3c3a8ff487cc692831b427
Rhyine <rhyinestewart@gmail.com>
2021-09-06 15:20:00 -0500

    Create NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import '../scss/style.scss';
+import { FaAngleLeft, FaMicrophone, FaCog } from "react-icons/fa";
+
+const NavBar = () => {
+    return (
+        <div className="NavBar-Container">
+            <p><FaAngleLeft /> 2021</p>
+            <p>Top 10 Market Capitalization</p>
+            <div>
+                <i><FaMicrophone /></i>
+                <i><FaCog /></i>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
