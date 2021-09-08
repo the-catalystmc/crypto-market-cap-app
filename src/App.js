844b888d7dce1d194ad626730815964d377bcf6a
Rhyine <rhyinestewart@gmail.com>
2021-09-08 09:13:09 -0500

    Refactor details routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  Route,
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
+  Route,
   Redirect,
-} from 'react-router-dom';
+} from "react-router-dom";
 import NavBar from './components/NavBar';
 import Currencies from './components/cryptos/Currencies';
+import Currency from './components/cryptos/Currency';
+import Header from './components/Header';
 import Stats from './components/Stats';
+// import { fetchCurrency } from "./redux/currencies";
 
 function App() {
+  // const allCurrencies = useSelector((state) => state.currency.currency);
+  // const currencyStatus = useSelector((state) => state.currency.loading);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   if (currencyStatus === false) {
+  //     dispatch(fetchCurrency());
+  //   }
+  // }, []);
+
+  // const routes = (currencies) => currencies.map((currency) => (
+  // <Route 
+  // key={currency.id} 
+  // path={`/${currency.id}`}
+  // render={() => }
+  // >
+  //   <Currency key={currency.id} currency={currency} /></Route>));
+
   return (
     <Router>
       <NavBar />
@@ -17,9 +39,7 @@ function App() {
         <Route path="/currencies">
           <Currencies />
         </Route>
-        <Route path="/stats">
-          <Stats />
-        </Route>
+        <Route path="/details/:id" component={Stats} />
         <Redirect from="/" to="/currencies" />
       </Switch>
     </Router>
