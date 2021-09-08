d8e721148732c26da68a38028105ab336af9bc35
Rhyine <rhyinestewart@gmail.com>
2021-09-08 09:15:15 -0500

    Update currencies

--- src/redux/currencies.js
+++ src/redux/currencies.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const GET_CURRENCY_REQUEST = 'GET_CURRENCY_REQUEST';
 const GET_CURRENCY_SUCCESS = 'GET_CURRENCY_SUCCESS';
 const GET_CURRENCY_FAILURE = 'GET_CURRENCY_FAILURE';
+const HOMEPAGE_SUCCESS = 'HOMEPAGE_SUCCESS';
+const GET_STATS_REQUEST = 'GET_STATS_REQUEST';
+const STATPAGE_SUCCESS = 'STATPAGE_SUCCESS';
 
 export const getCurrencyRequest = () => ({
     type: GET_CURRENCY_REQUEST,
@@ -14,12 +17,26 @@ export const getCurrencyRequest = () => ({
   });
   
   export const getCurrencyFailure = (error) => ({
-    type: GET_ROCKET_FAILURE,
+    type: GET_CURRENCY_FAILURE,
     payload: error,
   });
+
+//   export const homePageSuccess = (item) => ({
+//     type: HOMEPAGE_SUCCESS,
+//     payload: item,
+//   });
+
+export const getStatRequest = () => ({
+    type: GET_STATS_REQUEST,
+  });
+
+  export const statPageSuccess = (payload) => ({
+    type: STATPAGE_SUCCESS,
+    payload,
+  });
   
-  export const fetchRockets = () => (dispatch) => {
-    dispatch(getRocketRequest);
+  export const fetchCurrency = () => (dispatch) => {
+    dispatch(getCurrencyRequest);
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
       .then((response) => {
         const newData = response.data;
@@ -32,14 +49,39 @@ export const getCurrencyRequest = () => ({
           marketCap: coin.market_cap,
           rank: coin.market_cap_rank,
           totalVolume: coin.total_volume,
-          high: coin.high_24hr,
-          low: coin.low_24hr,
+          high: coin.high_24h,
+          low: coin.low_24h,
         }));
-        dispatch(getRocketSuccess(currency));
+        dispatch(getCurrencySuccess(currency));
       })
       .catch((error) => {
         const errorMsg = error.message;
-        dispatch(getRocketFailure(errorMsg));
+        dispatch(getCurrencyFailure(errorMsg));
+      });
+  };
+
+  export const fetchStats = () => (dispatch) => {
+    dispatch(getCurrencyRequest);
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+      .then((response) => {
+        const newData = response.data;
+        const currency = newData.map((coin) => ({
+          id: coin.id,
+          symbol: coin.symbol,
+          name: coin.name,
+          image: coin.image,
+          price: coin.current_price,
+          marketCap: coin.market_cap,
+          rank: coin.market_cap_rank,
+          totalVolume: coin.total_volume,
+          high: coin.high_24h,
+          low: coin.low_24h,
+        }));
+        dispatch(getCurrencySuccess(currency));
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        dispatch(getCurrencyFailure(errorMsg));
       });
   };
   
@@ -47,6 +89,8 @@ export const getCurrencyRequest = () => ({
     loading: false,
     currency: [],
     error: '',
+    home: false,
+    stats: [],
   };
   
   const currencyReducer = (state = initialState, action) => {
@@ -68,6 +112,12 @@ export const getCurrencyRequest = () => ({
           currency: [],
           error: action.payload,
         };
+
+        case HOMEPAGE_SUCCESS:
+        return { ...state, home: true };
+
+        case STATPAGE_SUCCESS:
+            return { ...state, home: action.payload };
 
       default:
         return state;
