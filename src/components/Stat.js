651e36aba98900f3cf82703bf530a4d7d5892ed7
Rhyine <rhyinestewart@gmail.com>
2021-09-08 11:29:58 -0500

    Pass correct statistics to stats page

--- src/components/Stat.js
+++ src/components/Stat.js
@@ -1,10 +1,17 @@
 import { NavLink, Link } from 'react-router-dom';
 
-const Stat = () => {
+const Stat = ({ currency }) => {
+    const { rank, low, high, price, marketCap, totalVolume } = currency;
+
     return (
         <div className="Stat-Container">
-            <p>Stat Name</p>
-            <p>Stat Info</p>
+            <ul>
+                <li><p>Rank</p><p>{rank}</p></li>
+                <li><p>24 Hour Low</p><p>{low}</p></li>
+                <li><p>24 Hour High</p><p>{high}</p></li>
+                <li><p>Price</p><p>{price}</p></li>
+                <li><p>Total Volume</p><p>{totalVolume}</p></li>
+            </ul>
         </div>
     )
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,45 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Stat from "./Stat";
+import { fetchCurrency, statPageSuccess } from '../redux/currencies';
 
-const Stats = () => {
+const Stats = ({ match }) => {
+    const allCurrencies = useSelector((state) => state.currency.currency);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    useEffect(() => {
+        if (allCurrencies.length === 0) {
+            dispatch(fetchCurrency());
+        }
+        dispatch(statPageSuccess(true));
+    }, []);
+
+    const currency = allCurrencies.find(
+        (currency) => currency.id.toString() === match.params.id.toString(),
+    );
+
+    if (allCurrencies.length === 0) {
+        return <div>Loading...</div>;
+    }
     return (
-        <div className="Stats-Container">
-            <Stat />
+        <div>
+            <div className="Header-Container">
+                <div className="Header-Picture">
+                    <img src={currency.image} alt="Logo" />
+                </div>
+                <div className="Header-Name">
+                    <h2>{currency.name.toUpperCase()}</h2>
+                    <p>{currency.marketCap} <br /> Market Capitalization</p>
+                </div>
+            </div>
+            <div>
+                <h3>STATS BY CURRENCY</h3>
+                <Stat currency={currency} />
+            </div>
         </div>
     )
 }
